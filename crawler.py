49d9fc9084475a262160ceef09769a45dff6e8b8
Mikhail Minulln <fikafusik@gmail.com>
2022-02-25 22:09:47 +0300

    rename nonogram fields

--- crawler.py
+++ crawler.py
@@ -29,12 +29,12 @@ def get_nonogram_from_html(html):
 
     nonogram_table = bs.find("table", { "id": "nonogram_table", "class": "nonogram_table" })
 
-    # Nonogram columns block
+    # Nonogram verticals block
     nmtt_body = nonogram_table.find("td", { "class": "nmtt" }).find("table").find("tbody")
 
-    # Count of all `td` in any `tr` of nmtt_body is a count of columns
-    columns_count = len(nmtt_body.find("tr").findAll("td"))
-    columns = [ [] for _ in range(columns_count) ]
+    # Count of all `td` in any `tr` of nmtt_body is a count of verticals
+    verticals_count = len(nmtt_body.find("tr").findAll("td"))
+    verticals = [ [] for _ in range(verticals_count) ]
 
     nmtt_trs = nmtt_body.findAll("tr")
     for tr in nmtt_trs:
@@ -42,25 +42,25 @@ def get_nonogram_from_html(html):
         for i in range(len(nmtt_tds)):
             number = parse_td(nmtt_tds[i])
             if number != 0:
-                columns[i].append(number)
+                verticals[i].append(number)
 
-    # Nonogram rows block
+    # Nonogram horizontals block
     nmtl_body = nonogram_table.find("td", { "class": "nmtl" }).find("table").find("tbody")
 
     nmtl_trs = nmtl_body.findAll("tr")
 
-    # Count of all `tr` of nmtl_body is a count of rows
-    rows_count = len(nmtl_trs)
-    rows = [ [] for _ in range(rows_count) ]
+    # Count of all `tr` of nmtl_body is a count of horizontals
+    horizontals_count = len(nmtl_trs)
+    horizontals = [ [] for _ in range(horizontals_count) ]
 
-    for i in range(rows_count):
+    for i in range(horizontals_count):
         tr = nmtl_trs[i]
         for td in tr:
             number = parse_td(td)
             if number != 0:
-                rows[i].append(number)
+                horizontals[i].append(number)
 
-    return { 'columns': columns, 'rows': rows }
+    return { 'verticals': verticals, 'horizontals': horizontals }
 
 def main():
     NONOGRAM_MAX_INDEX = 54216
